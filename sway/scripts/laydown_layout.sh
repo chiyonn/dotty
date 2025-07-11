589057e6e303ffc5339600902752e6ccd31843df
chiyonn <kud0h.chi4@gmail.com>
2025-07-11 16:46:12 +0900

    disable: deleted display layout things

--- sway/scripts/laydown_layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-swaymsg 'output eDP-1 scale 1.0'
-swaymsg 'output HDMI-A-1 scale 0.8'
-swaymsg 'output eDP-1 pos 0 1355 res 2560x1440'
-swaymsg 'output HDMI-A-1 pos 0 0 res 2560x1080'
--- sway/scripts/sitting_layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-swaymsg 'output eDP-1 scale 1.0'
-swaymsg 'output HDMI-A-1 scale 1.0'
-swaymsg 'output eDP-1 pos 3840 720 res 2560x1440'
-swaymsg 'output HDMI-A-1 pos 0 0 res 3840x2160'
